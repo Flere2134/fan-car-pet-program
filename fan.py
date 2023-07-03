4302a11c3a55f9a720519b6cfc2ed5325499b429
Flere21 <aaronhans2134@gmail.com>
2023-07-03 23:14:35 +0800

    added the getter and setter of private data fields

--- fan.py
+++ fan.py
@@ -8,3 +8,20 @@ class Fan:
         self.__power = power
         self.__radius = radius
         self.__color = color
+
+    def get_speed(self):
+        return self.__speed
+    def set_speed(self, speed):
+        self.__speed = speed
+    def get_radius(self):
+        return self.__radius
+    def set_radius(self, radius):
+        self.__radius = radius
+    def get_color(self):
+        return self.__color
+    def set_color(self, color):
+        self.__color = color
+    def get_power(self):
+        return self.__power
+    def set_power(self, power):
+        self.__power = power
