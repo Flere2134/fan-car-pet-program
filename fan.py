5ee32eb69da4fe13cb4f3e39d786b3e655a39a35
Flere21 <aaronhans2134@gmail.com>
2023-07-03 23:10:38 +0800

    created constructor with specified parameters

--- fan.py
+++ fan.py
@@ -2,3 +2,9 @@ class Fan:
     slow = 1
     medium = 2
     fast = 3
+
+    def __init__(self, speed = slow, radius = 5, color = "blue", power = False):
+        self.__speed = speed
+        self.__power = power
+        self.__radius = radius
+        self.__color = color
