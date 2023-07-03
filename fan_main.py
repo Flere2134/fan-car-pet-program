bac05f134a9ced296f4904b33f71549d562e1784
Flere21 <aaronhans2134@gmail.com>
2023-07-03 23:59:44 +0800

    created main fan program and changed little details

---
+++ fan_main.py
@@ -0,0 +1,6 @@
+from fan import Fan
+from test_fan import TestFan
+
+fan = TestFan
+
+fan.main_fan(self = Fan)
--- test_fan.py
+++ test_fan.py
@@ -1,15 +1,15 @@
 from fan import Fan
 
 class TestFan:
-    def main(self):
+    def main_fan(self):
         fan_num_1 = Fan(Fan.fast, 10, "yellow", True)
         fan_num_2 = Fan(Fan.medium, 5, "blue", False)
     
         print("Fan 1 speed is", fan_num_1.get_speed())
         print("Fan 1 radius is", fan_num_1.get_radius())
         print("Fan 1 color is", fan_num_1.get_color())
-        print("Fan 1 is turned on", fan_num_1.get_power())
+        print("is Fan 1 on:", fan_num_1.get_power())
         print("Fan 2 speed is", fan_num_2.get_speed())
         print("Fan 2 radius is", fan_num_2.get_radius())
         print("Fan 2 color is", fan_num_2.get_color())
-        print("Fan 2 is turned on", fan_num_2.get_power())
+        print("is Fan 2 on:", fan_num_2.get_power())
