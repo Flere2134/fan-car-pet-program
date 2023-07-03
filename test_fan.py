11be6e113036640f32c88e7bbad58a56f53326f3
Flere21 <aaronhans2134@gmail.com>
2023-07-03 23:43:54 +0800

    displayed Fan 1's status

--- test_fan.py
+++ test_fan.py
@@ -1,7 +1,11 @@
 from fan import Fan
 
 class TestFan:
-    def main_fan(self):
+    def main(self):
         fan_num_1 = Fan(Fan.fast, 10, "yellow", True)
         fan_num_2 = Fan(Fan.medium, 5, "blue", False)
     
+        print("Fan 1 speed is", fan_num_1.get_speed())
+        print("Fan 1 radius is", fan_num_1.get_radius())
+        print("Fan 1 color is", fan_num_1.get_color())
+        print("Fan 1 is turned on", fan_num_1.get_power())
