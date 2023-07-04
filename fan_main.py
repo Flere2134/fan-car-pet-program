04bec8f41a2cd3b36d5d08c4f0b49bcf90d1d9cc
Flere21 <aaronhans2134@gmail.com>
2023-07-04 17:37:51 +0800

    created Pet object and get user pet info

--- fan_main.py
+++ fan_main.py
@@ -1,6 +1,6 @@
 from fan import Fan
 from test_fan import TestFan
 
-fan = TestFan
+fan = TestFan()
 
-fan.main_fan(self = Fan)
+fan.main_fan()
--- pet_main.py
+++ pet_main.py
@@ -1,2 +1,6 @@
 from pet import Pet
 from pet_ui import PetUserInterface
+
+pet = Pet()
+pet_name, pet_animal_type, pet_age = PetUserInterface.ask_user_pet_info()
+
