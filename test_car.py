5896964ddfc2c7518e33a97ff8acaa816e0b7915
Flere21 <aaronhans2134@gmail.com>
2023-07-04 17:13:22 +0800

    called accelerate and brake method 5 times and displayed each speed status for every loop

--- test_car.py
+++ test_car.py
@@ -1,3 +1,11 @@
 from car import Car
 
 car = Car("2023", "Nissan")
+
+for i in range(5):
+    car.accelerator()
+    print("\nCar is accelerating. Showing current speed:", car.get_speed())
+
+for i in range(5):
+    car.brake()
+    print("\nCar is slowly losing speed. Showing current speed:", car.get_speed())
