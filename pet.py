48d2cef7915cc5605f18894f5f56b1ca7a75b3b2
Flere21 <aaronhans2134@gmail.com>
2023-07-04 17:24:36 +0800

    added getter methods

--- pet.py
+++ pet.py
@@ -9,3 +9,9 @@ class Pet:
         self.__animal_type = animal_type
     def set_age(self, age):
         self.__age = age
+    def get_name(self, name):
+        return self.__name
+    def get_animal_type(self, animal_type):
+        return self.__animal_type
+    def get_age(self, age):
+        return self.__age
