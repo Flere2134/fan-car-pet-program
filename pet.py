f730933b3eef7eddf6cb4ec9cc5a005f769349ed
Flere21 <aaronhans2134@gmail.com>
2023-07-04 17:20:13 +0800

    revised my instructor and added setter methods

--- pet.py
+++ pet.py
@@ -1,5 +1,11 @@
 class Pet:
     def __init__(self, name, animal_type, age):
+        self.__name = ''
+        self.__animal_type = ''
+        self.__age = ''
+    def set_name(self, name):
         self.__name = name
+    def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
+    def set_age(self, age):
         self.__age = age
